fd129c3e7d1d9ada9aba5cf5f7c1d3d05a318775
kammitama5 <krystal.maughan@gmail.com>
2016-10-26 14:03:04 -0700

    Update _004.rkt

--- _004.rkt
+++ _004.rkt
@@ -1,5 +1,6 @@
 #lang racket
 ;;define a programme that consumes Farenheit and returns Celsius
+;; this was based on a quick google search. It is not accurate -> see _004a for more accurate solution
 (define (farenheit_celsius x)
   (- x 32))
 
