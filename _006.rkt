10864e285847c244fbab44438b0c1c6d09217bca
kammitama5 <krystal.maughan@gmail.com>
2016-10-26 12:14:00 -0700

    1st commit

---
+++ _006.rkt
@@ -0,0 +1,11 @@
+#lang racket
+;;define triangle. Calculates area of triangle
+;;it consumes the length of side and perpendicular height
+
+
+(define(area a b)
+  (/ (* a b)2))
+
+(area 3 4)
+(area 10 5)
+(area 2 5)
