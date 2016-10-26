3dce12a437470ef1cd36d1a11a1cfab6bd54eebd
kammitama5 <krystal.maughan@gmail.com>
2016-10-26 14:22:05 -0700

    1st commit

---
+++ _014.rkt
@@ -0,0 +1,13 @@
+#lang racket
+;;An old-style movie-theater has a simple profit function
+;;Each customer pays $5 per ticket
+;;Each performance costs the theater $20
+;;plus $.50 per attendee
+;;Develop the total-profit where input is number of attendees
+
+(define (movieprofit num)
+ (- (* 5 num)(+ (*(/ 1 2) num)20)))
+
+(movieprofit 100) ;; prints $430
+(movieprofit 5) ;; prints $2.50
+(movieprofit 2) ;; prints -$11.00
