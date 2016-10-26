3acf4475d09a8b7e54b2bcdab93b19d44e75f814
kammitama5 <krystal.maughan@gmail.com>
2016-10-26 11:01:34 -0700

    1st commit

---
+++ _002.rkt
@@ -0,0 +1,11 @@
+#lang racket
+(define (extract str)
+  (substring str 4 7))
+
+(extract "boy out of country") ;; prints boy
+(* (+ 2 2)(/ (* (+ 3 5) (/ 30 10)) 2)) ;; prints 48
+(expt 3 5) ;; prints 243
+(sqrt 25) ;; prints 5
+(remainder 10 2) ;; prints 0
+(log 5) ;; prints 1.6094379124341003
+(sin 1.5) ;; radians -> prints 0.9974949866040545
